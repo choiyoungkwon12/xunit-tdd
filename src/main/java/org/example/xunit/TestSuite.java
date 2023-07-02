b97c80e72bb32695dafb94807c23b1ea7d2231fb
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-03 02:35:22 +0900

    수동으로 테스트 메서드를 추가해서 실행하는 것이 아닌 관례로 테스트 케이스의 메서드 명 이름이 test 로 시작하는 메서드를 실행하도록 변경

--- src/main/java/org/example/xunit/TestSuite.java
+++ src/main/java/org/example/xunit/TestSuite.java
@@ -1,10 +1,27 @@
 package org.example.xunit;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestSuite implements Test {
     List<Test> tests = new ArrayList<>();
+
+    public TestSuite(Class<? extends TestCase> testClass) {
+        Arrays.stream(testClass.getMethods())
+                .filter(method -> method.getName()
+                .startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        add(testClass.getConstructor(String.class).newInstance(method.getName()));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public TestSuite() {}
 
     public void add(Test testMethod) {
         tests.add(testMethod);
