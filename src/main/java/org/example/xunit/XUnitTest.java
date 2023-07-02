7072b6eb12b4fef8ec8e8da17caa3d45e2a8bd30
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-02 21:18:37 +0900

    테스트 케이스를 상속한 테스트케이스테스트 사용

--- src/main/java/org/example/xunit/XUnitTest.java
+++ src/main/java/org/example/xunit/XUnitTest.java
@@ -2,11 +2,6 @@ package org.example.xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        // 33분 까지 들음~~ https://www.youtube.com/watch?v=tdKFZcZSJmg&list=PLv-xDnFD-nnmof-yoZQN8Fs2kVljIuFyC&index=18
-        WasRun wasRun = new WasRun("testMethod");
-        System.out.println(wasRun.wasRun); // false
-        wasRun.run();
-        System.out.println(wasRun.wasRun); // true
-
+        new TestCaseTest("testRunning").run();
     }
 }
