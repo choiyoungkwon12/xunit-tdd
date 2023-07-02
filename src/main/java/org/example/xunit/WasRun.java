bb79bcdad88282a77160fa4858528d1c4386829f
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-02 21:05:17 +0900

    testMethod 실행 대신 run 메서드 실행하면 testMethod 실행되도록 수정(실패함)

--- src/main/java/org/example/xunit/WasRun.java
+++ src/main/java/org/example/xunit/WasRun.java
@@ -3,7 +3,15 @@ package org.example.xunit;
 public class WasRun {
     public boolean wasRun;
 
+    public WasRun(String testMethod) {
+
+    }
+
     public void testMethod() {
         this.wasRun = true;
+    }
+
+    public void run() {
+
     }
 }
--- src/main/java/org/example/xunit/XUnitTest.java
+++ src/main/java/org/example/xunit/XUnitTest.java
@@ -2,10 +2,10 @@ package org.example.xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        // 29: 35
-        WasRun wasRun = new WasRun();
+        // 33분 까지 들음~~ https://www.youtube.com/watch?v=tdKFZcZSJmg&list=PLv-xDnFD-nnmof-yoZQN8Fs2kVljIuFyC&index=18
+        WasRun wasRun = new WasRun("testMethod");
         System.out.println(wasRun.wasRun); // false
-        wasRun.testMethod();
+        wasRun.run();
         System.out.println(wasRun.wasRun); // true
 
     }
